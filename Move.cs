d80cb3ed8b0d2846ffef382a5b261e3acf15e6f4
Bingboygogo <2621396965@qq.com>
2017-10-10 20:41:26 +0800

    Mathf
    新增SmoothDamp()和SmoothStep()

--- Move.cs
+++ Move.cs
@@ -15,32 +15,45 @@ public class Move : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        t = 0.5f;
     }
 
     void FixedUpdate(){
         // Mathf.Clamp() 限制输入的范围
         t = Mathf.Clamp(t, 0, 4);
         startValue = Mathf.Clamp(startValue, 0, 10);
-        endValue = Mathf.Clamp(endValue, 0, 20);
+        endValue = Mathf.Clamp(endValue, startValue, 20);
+        currentX = this.transform.position.x;
 
-        // lerp()
+        // lerp() 设置T 
         //movedir = Mathf.Lerp(startValue, endValue, Time.time - t);
         //this.transform.position = new Vector3(movedir, this.transform.position.y, this.transform.position.z);
 
+        // SmoothStep() 设置T, endValue
+        this.transform.position = new Vector3(Mathf.SmoothStep(this.transform.position.x, endValue, Time.time - t),
+                                                this.transform.position.y,
+                                                this.transform.position.z);
+
+        // SmoothDamp()  设置T,speed,target
+        //this.transform.position = new Vector3(Mathf.SmoothDamp(this.transform.position.x, target,ref speed, t, 10.0f, Time.deltaTime),
+        //                                        this.transform.position.y,
+        //                                        this.transform.position.z);
+
         // MoveToward()
-        currentX = this.transform.position.x;
         //// 以下会持续运动
         //target = new Vector3(this.transform.position.x + 0.1f, this.transform.position.y, this.transform.position.z);
         //currentX = Mathf.MoveTowards(currentX, this.transform.position.x + 0.1f, 1.0f * Time.deltaTime);
         //this.transform.position = new Vector3(currentX,this.transform.position.y, this.transform.position.z);
         // 以下会移动到某个地点
-        this.transform.position = new Vector3(Mathf.MoveTowards(this.transform.position.x, target, speed * Time.deltaTime), 
-                                                this.transform.position.y,
-                                                this.transform.position.z);
+        //this.transform.position = new Vector3(Mathf.MoveTowards(this.transform.position.x, target, speed * Time.deltaTime), 
+        //                                        this.transform.position.y,
+        //                                        this.transform.position.z);
+
     }
 
     // Update is called once per frame
     void Update () {
 		
 	}
+
 }

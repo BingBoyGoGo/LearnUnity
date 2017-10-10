07b76bbc5874ff694a3a48c3d6e09f7ce63a5c1a
Bingboygogo <2621396965@qq.com>
2017-10-10 20:47:27 +0800

    Mathf
    使用PerlinNoise()改变物体颜色

---
+++ test_PerlinNoise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_PerlinNoise : MonoBehaviour {
+
+    Renderer rend;
+    Vector3 ColorSpace;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        ColorSpace = new Vector3(
+                        Random.Range(0, 10),
+                        Random.Range(0, 10),
+                        Random.Range(0, 10));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //this.rend.material.color = new Color(Mathf.PerlinNoise(Time.time, ColorSpace.x),
+        //                                     Mathf.PerlinNoise(Time.time, ColorSpace.y),
+        //                                     Mathf.PerlinNoise(Time.time, ColorSpace.z));
+
+        // 不使用PerlinNoise()的对比
+        this.rend.material.color = new Color(Random.value, Random.value, Random.value);
+	}
+}

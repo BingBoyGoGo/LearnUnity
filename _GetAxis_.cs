e74197666ac141ac47e032f00f323c94d31d5a90
Bingboygogo <2621396965@qq.com>
2017-10-16 23:05:16 +0800

    GetAxis
    使用Input面板，观察各个选项变化的影响

---
+++ _GetAxis_.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _GetAxis_ : MonoBehaviour {
+
+    float axisX;
+    float axisY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        axisX = Input.GetAxis("Horizontal");
+        axisY = Input.GetAxis("Vertical");
+
+        this.transform.position = new Vector3(axisX, axisY, 0);
+	}
+}
